a9196988dafe4168fd958dc50da3a7d93ab68acf
Fuvad <fuvad777@gmail.com>
2026-06-06 21:17:37 +0530

    Convert frontend to regular directory

---
+++ frontend/app/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <main className="p-10">
+      <h1 className="text-3xl font-bold">
+        Enterprise Knowledge Assistant
+      </h1>
+
+      <p>
+        Day 1 Frontend Setup Complete
+      </p>
+    </main>
+  );
+}
